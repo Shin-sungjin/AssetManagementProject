b761925e40b826ebd843b8192772da648ba7cb0e
sjshin <tlstdjwls0906@gmail.com>
2024-11-02 00:18:23 +0900

    web-xml 초기설정

---
+++ AssetManagementProject/src/main/java/com/users/controller/main.java
@@ -0,0 +1,14 @@
+package com.users.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/AssetManagementProjec")
+public class main {
+
+     @RequestMapping(value="/")
+    public void mainTest(){
+
+    }
+}
---
+++ AssetManagementProject/src/main/java/com/users/service/userService.java
@@ -0,0 +1,4 @@
+package com.users.service;
+
+interface userService {
+}
