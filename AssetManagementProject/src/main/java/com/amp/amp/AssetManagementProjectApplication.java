76c9b22b17dd3cdc91ddcdcee0dce79e67a7128e
ad-seoil <ad.seoil0112@gmail.com>
2024-11-01 22:59:24 +0900

    20241101 HSJ GitTest

---
+++ AssetManagementProject/src/main/java/com/amp/amp/AssetManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.amp.amp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagementProjectApplication.class, args);
+	}
+
+}
